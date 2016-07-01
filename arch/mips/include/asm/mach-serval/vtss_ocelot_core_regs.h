f367b652b18cd02a9ed8381223624b188ddddc48
Rene Nielsen <rene.nielsen@microsemi.com>
2016-07-01 16:00:00 +0200

    Changed copyright from Vitesse to Microsemi on files that FJ missed in first round

--- arch/mips/include/asm/mach-serval/vtss_ocelot_core_regs.h
+++ arch/mips/include/asm/mach-serval/vtss_ocelot_core_regs.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2015 Microsemi Corporation
+/* Copyright (c) 2016 Microsemi Corporation
 
    Permission is hereby granted, free of charge, to any person obtaining a copy
    of this software and associated documentation files (the "Software"), to deal
