4164608bc3e1a0552207b704e49cbfceec44fff3
Lars Povlsen <lars.povlsen@microsemi.com>
2016-10-03 16:48:52 +0200

    MSCC: Jr2/Serval2 mx35: Increase SPI NAND speed to 104MhZ

--- arch/mips/jaguar2/target_mtd.c
+++ arch/mips/jaguar2/target_mtd.c
@@ -148,7 +148,7 @@ static struct spi_board_info jaguar2_spi_board_info[] __initdata = {
 	{
 		/* the modalias must be the same as spi device driver name */
 		.modalias = "mx35", /* Name of spi_driver for this device */
-		.max_speed_hz = 50000000,     /* max spi clock (SCK) speed in HZ */
+		.max_speed_hz = 104000000,     /* max spi clock (SCK) speed in HZ */
 		.bus_num = 0, /* Framework bus number */
 		.chip_select = SPINAND_CS, /* Framework chip select. */
 		.platform_data = &jaguar2_spinand_flash_data,
