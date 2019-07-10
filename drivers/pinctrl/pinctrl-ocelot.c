945cc1524dec87c0f5b5df5f5bf605b5f299540a
Lars Povlsen <lars.povlsen@microchip.com>
2019-07-10 15:16:56 +0200

    pinctrl: fireant: Fix warning

--- drivers/pinctrl/pinctrl-ocelot.c
+++ drivers/pinctrl/pinctrl-ocelot.c
@@ -1011,7 +1011,7 @@ static int ocelot_pinconf_get(struct pinctrl_dev *pctldev,
 
 	case PIN_CONFIG_INPUT_ENABLE:
 	case PIN_CONFIG_OUTPUT_ENABLE:
-		regmap_read(info->map, REG(OCELOT_GPIO_OE, info, pin), &val);
+		err = regmap_read(info->map, REG(OCELOT_GPIO_OE, info, pin), &val);
 		if (err)
 			return err;
 
@@ -1021,6 +1021,7 @@ static int ocelot_pinconf_get(struct pinctrl_dev *pctldev,
 		else
 			val = !val;
 		break;
+
 	default:
 		return -ENOTSUPP;
 	}
