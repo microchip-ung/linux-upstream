24b5aeb0ab254b9c3169e6c2d20fccfd68fe5057
Lars Povlsen <lars.povlsen@microchip.com>
2019-10-24 16:26:50 +0200

    fireant: clock: Added actual calculation of LCPLL params

--- drivers/clk/clk-fireant.c
+++ drivers/clk/clk-fireant.c
@@ -7,7 +7,6 @@
  * Author: Lars Povlsen <lars.povlsen@microchip.com>
  */
 
-#define DEBUG
 #include <linux/io.h>
 #include <linux/clk-provider.h>
 #include <linux/of.h>
@@ -16,19 +15,24 @@
 #include <linux/platform_device.h>
 #include <dt-bindings/clock/mchp,fireant.h>
 
-#define PLL_DIV_MASK		GENMASK(7,0)
-#define PLL_PRE_DIV_MASK	GENMASK(10,8)
+#define PLL_DIV_MASK		GENMASK(7, 0)
+#define PLL_PRE_DIV_MASK	GENMASK(10, 8)
 #define PLL_PRE_DIV_SHIFT	8
 #define PLL_ROT_DIR		BIT(11)
-#define PLL_ROT_SEL_MASK	GENMASK(13,12)
+#define PLL_ROT_SEL_MASK	GENMASK(13, 12)
 #define PLL_ROT_SEL_SHIFT	12
 #define PLL_ROT_ENA		BIT(14)
 #define PLL_CLK_ENA		BIT(15)
 
+#define MAX_SEL 4
+#define MAX_PRE BIT(3)
+
 #define KHZ 1000
 #define MHZ (KHZ*KHZ)
 
-#define BASE_CLOCK (2500*MHZ)
+#define BASE_CLOCK (2500UL*MHZ)
+
+static u8 sel_rates[MAX_SEL] = { 0, 2*8, 2*4, 2*2 };
 
 static const char *clk_names[N_CLOCKS] = {
 	"core", "ddr", "cpu2", "arm2",
@@ -47,22 +51,98 @@ struct fa_clk_data {
 	struct fa_hw_clk fa_hw[N_CLOCKS];
 };
 
+struct pll_conf {
+	int freq;
+	u8 div;
+	bool rot_ena;
+	u8 rot_sel;
+	u8 rot_dir;
+	u8 pre_div;
+};
+
 #define to_clk_pll(hw) container_of(hw, struct fa_hw_clk, hw)
 
-static int clk_calc_params(unsigned long rate,
-			   unsigned long parent_rate,
-			   int *div,
-			   int *pre_div,
-			   int *rot_sel)
+unsigned long calc_freq(const struct pll_conf *pdata)
 {
-	/* Cheat */
-	if (rate == 100*MHZ) {
-		*div = 3;
-		*pre_div = 2;
-		*rot_sel = 2;
-		return 0;
+	unsigned long rate = BASE_CLOCK / pdata->div;
+
+	if (pdata->rot_ena) {
+		unsigned long base = BASE_CLOCK / pdata->div;
+		int sign = pdata->rot_dir ? -1 : 1;
+		int divt = sel_rates[pdata->rot_sel] * (1 + pdata->pre_div);
+		int divb = divt + sign;
+
+		rate = mult_frac(base, divt, divb);
+		rate = roundup(rate, 1000);
 	}
-	return -ENOTSUPP;
+
+	return rate;
+}
+
+static unsigned long clk_calc_params(unsigned long rate,
+				     struct pll_conf *conf)
+{
+	memset(conf, 0, sizeof(*conf));
+
+	conf->div = DIV_ROUND_CLOSEST_ULL(BASE_CLOCK, rate);
+
+	if (BASE_CLOCK % rate) {
+		struct pll_conf best;
+		ulong cur_offset, best_offset = rate;
+		int i, j;
+
+		/* Enable fractional rotation */
+		conf->rot_ena = true;
+
+		if ((BASE_CLOCK / rate) != conf->div) {
+			/* Overshoot, adjust other direction */
+			conf->rot_dir = 1;
+		}
+
+		/* Brute force search over MAX_PRE * (MAX_SEL - 1) = 24 */
+		for (i = 0; i < MAX_PRE; i++) {
+			conf->pre_div = i;
+			for (j = 1; j < MAX_SEL; j++) {
+				conf->rot_sel = j;
+				conf->freq = calc_freq(conf);
+				cur_offset = abs(rate - conf->freq);
+				if (cur_offset == 0)
+					/* Perfect fit */
+					goto done;
+				if (cur_offset < best_offset) {
+					/* Better fit found */
+					best_offset = cur_offset;
+					best = *conf;
+				}
+			}
+		}
+		/* Best match */
+		*conf = best;
+	}
+
+done:
+	return conf->freq;
+}
+
+static int clk_pll_enable(struct clk_hw *hw)
+{
+	struct fa_hw_clk *pll = to_clk_pll(hw);
+	u32 val = readl(pll->reg);
+
+	val |= PLL_CLK_ENA;
+	writel(val, pll->reg);
+	pr_debug("%s: Enable val %04x\n", clk_names[pll->index], val);
+	return 0;
+}
+
+static void clk_pll_disable(struct clk_hw *hw)
+{
+	struct fa_hw_clk *pll = to_clk_pll(hw);
+	u32 val = readl(pll->reg);
+
+	val &= ~PLL_CLK_ENA;
+	writel(val, pll->reg);
+	pr_debug("%s: Disable val %04x\n", clk_names[pll->index], val);
 }
 
 static int clk_pll_set_rate(struct clk_hw *hw,
@@ -70,20 +150,32 @@ static int clk_pll_set_rate(struct clk_hw *hw,
 			    unsigned long parent_rate)
 {
 	struct fa_hw_clk *pll = to_clk_pll(hw);
-	int ret;
-	int div, pre_div, rot_sel;
+	struct pll_conf conf;
+	unsigned long eff_rate;
+	int ret = 0;
 
-	ret = clk_calc_params(rate, parent_rate,
-			      &div, &pre_div, &rot_sel);
-	if (!ret) {
-		u32 val = PLL_CLK_ENA | PLL_ROT_ENA |
-			(PLL_ROT_SEL_MASK & (rot_sel << PLL_ROT_SEL_SHIFT)) |
-			(PLL_PRE_DIV_MASK & (pre_div << PLL_PRE_DIV_SHIFT)) |
-			(PLL_DIV_MASK & div);
+	eff_rate = clk_calc_params(rate, &conf);
+	if (eff_rate == rate) {
+		u32 val;
+
+		val = readl(pll->reg) & PLL_CLK_ENA;
+		val |= PLL_DIV_MASK & conf.div;
+		if (conf.rot_ena) {
+			val |= (PLL_ROT_ENA |
+				(PLL_ROT_SEL_MASK &
+				 (conf.rot_sel << PLL_ROT_SEL_SHIFT)) |
+				(PLL_PRE_DIV_MASK &
+				 (conf.pre_div << PLL_PRE_DIV_SHIFT)));
+			if (conf.rot_dir)
+				val |= PLL_ROT_DIR;
+		}
+		pr_debug("%s: Rate %ld >= 0x%04x\n",
+			 clk_names[pll->index], rate, val);
 		writel(val, pll->reg);
 	} else {
-		pr_err("%s: clock %d unsupported: %ld paren %ld\n",
-		       clk_names[pll->index], pll->index, rate, parent_rate);
+		pr_err("%s: freq unsupported: %ld paren %ld\n",
+		       clk_names[pll->index], rate, parent_rate);
+		ret = -ENOTSUPP;
 	}
 
 	return ret;
@@ -99,14 +191,21 @@ static unsigned long clk_pll_recalc_rate(struct clk_hw *hw,
 static long clk_pll_round_rate(struct clk_hw *hw, unsigned long rate,
 			       unsigned long *parent_rate)
 {
-	/* 1:1 */
-	return rate;
+	struct pll_conf conf;
+	unsigned long eff_rate;
+
+	eff_rate = clk_calc_params(rate, &conf);
+	pr_debug("%s: Rate %ld rounded to %ld\n", __func__, rate, eff_rate);
+
+	return eff_rate;
 }
 
 static const struct clk_ops fa_pll_ops = {
-	.set_rate    = clk_pll_set_rate,
-	.round_rate  = clk_pll_round_rate,
-	.recalc_rate = clk_pll_recalc_rate,
+	.enable		= clk_pll_enable,
+	.disable	= clk_pll_disable,
+	.set_rate	= clk_pll_set_rate,
+	.round_rate	= clk_pll_round_rate,
+	.recalc_rate	= clk_pll_recalc_rate,
 };
 
 static struct fa_clk_data *fa_clk_alloc(struct device_node *np)
@@ -153,6 +252,7 @@ static void __init fa_pll_init(struct device_node *np)
 
 	for (i = 0; i < N_CLOCKS; i++) {
 		struct fa_hw_clk *fa_hw = &pll_clk->fa_hw[i];
+
 		init.name = clk_names[i];
 		fa_hw->index = i;
 		fa_hw->reg = pll_clk->base + (i * sizeof(u32));
