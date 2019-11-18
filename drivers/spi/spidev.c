bcdb3aca81bf9c413389ce453f696aeef3d94702
Lars Povlsen <lars.povlsen@microchip.com>
2019-11-18 13:29:36 +0100

    fireant: Add support for redwood ptp and synce module

--- drivers/spi/spidev.c
+++ drivers/spi/spidev.c
@@ -673,7 +673,8 @@ static const struct of_device_id spidev_dt_ids[] = {
 	{ .compatible = "lwn,bk4" },
 	{ .compatible = "dh,dhcom-board" },
 	{ .compatible = "menlo,m53cpld" },
-	{ .compatible = "spidev" },
+	{ .compatible = "mchp,redwood" },
+	{ .compatible = "mchp,synce" },
 	{},
 };
 MODULE_DEVICE_TABLE(of, spidev_dt_ids);
