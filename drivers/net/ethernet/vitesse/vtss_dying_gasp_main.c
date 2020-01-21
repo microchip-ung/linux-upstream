6018c5fa071022b2b642cafadc0b79dc4c8d4ef6
Lars Povlsen <lars.povlsen@microchip.com>
2020-01-21 10:54:25 +0100

    net: vitesse: dying_gasp: Clean up driver, change compatible string to 'mscc,dying-gasp', add to mscc_vcoreiii_defconfig

--- drivers/net/ethernet/vitesse/vtss_dying_gasp_main.c
+++ drivers/net/ethernet/vitesse/vtss_dying_gasp_main.c
@@ -1,23 +1,8 @@
-/* Copyright (c) 2015 Microsemi Corporation
-
-   Permission is hereby granted, free of charge, to any person obtaining a copy
-   of this software and associated documentation files (the "Software"), to deal
-   in the Software without restriction, including without limitation the rights
-   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-   copies of the Software, and to permit persons to whom the Software is
-   furnished to do so, subject to the following conditions:
-
-   The above copyright notice and this permission notice shall be included in
-   all copies or substantial portions of the Software.
-
-   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
-   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-   THE SOFTWARE.
-*/
+// SPDX-License-Identifier: (GPL-2.0 OR MIT)
+/* Microsemi dying gasp driver
+ * Author: Wenxi Jin <wenxi.jin@microsemi.com>
+ * Copyright (c) 2015 Microsemi Corporation
+ */
 
 #include <linux/kernel.h>
 #include <linux/module.h>
@@ -46,27 +31,30 @@ static void vtss_dying_gasp_tasklet(unsigned long data)
 	struct vtss_dying_gasp_genl_buf *b;
 	struct sk_buff *skb_dying_gasp;
 
-	/* Get vtss.ifh in the isr, so there is no dependency on the order
-	 * of module initialization */
-	if (!priv->ndev) {
+	/* Get vtss.ifh in the isr, so there is no dependency on the
+	 * order of module initialization
+	 */
+	if (!priv->ndev)
 		priv->ndev = dev_get_by_name(&init_net, "vtss.ifh");
-	}
 
-	pr_debug("%s: net_device: %px\n", __func__, priv->ndev);
+	pr_debug("%s: net_device: %p\n", __func__, priv->ndev);
 	/* Send dying gasp pdu only if vtss.ifh interface is available */
 	if (priv->ndev && priv->ndev->netdev_ops->ndo_start_xmit) {
 		rcu_read_lock();
-		list_for_each_entry_rcu (b, &VTSS_DYING_GASP_GENL_BUF, list) {
+		list_for_each_entry_rcu(b, &VTSS_DYING_GASP_GENL_BUF, list) {
 			/* allocate a sk_buff for dying gasp buf */
 			skb_dying_gasp = alloc_skb(ETH_FRAME_LEN, GFP_ATOMIC);
 			if (skb_dying_gasp) {
 				skb_dying_gasp->len = b->msg_len;
-				memcpy(skb_dying_gasp->data, (unsigned char *)b->msg, b->msg_len);
+				memcpy(skb_dying_gasp->data, b->msg,
+				       b->msg_len);
 
-				pr_debug("%s: TX: %px: skb: %px\n", __func__, priv->ndev, skb_dying_gasp);
-				priv->ndev->netdev_ops->ndo_start_xmit(
-					skb_dying_gasp, priv->ndev);
-				/* skb_dying_gasp will be freed after transmit */
+				pr_debug("%s: TX: %p: skb: %p\n",
+					 __func__, priv->ndev, skb_dying_gasp);
+				priv->ndev->netdev_ops->ndo_start_xmit
+					(skb_dying_gasp,
+					 priv->ndev);
+				/* skb_dying_gasp freed after transmit */
 			}
 		}
 		rcu_read_unlock();
@@ -109,16 +97,12 @@ static int vtss_dying_gasp_probe(struct platform_device *pdev)
 
 	irq = platform_get_irq(pdev, 0);
 	ret = devm_request_irq(&pdev->dev, irq, vtss_dying_gasp_isr, 0,
-		dev_name(&pdev->dev), priv);
+			       dev_name(&pdev->dev), priv);
 	if (ret) {
 		dev_err(&pdev->dev, "Failed to register IRQ %d\n", irq);
 		goto exit;
 	}
 
-#if defined(CONFIG_VTSS_VCOREIII_SERVALT)
-	/* PCB116: GPIO#04, ALT"01", IRQ0_IN */
-	vcoreiii_gpio_set_mode( 4, 1);
-#endif
 	tasklet_init(&priv->tasklet, vtss_dying_gasp_tasklet,
 		     (unsigned long)priv);
 
@@ -138,7 +122,7 @@ static int vtss_dying_gasp_remove(struct platform_device *pdev)
 }
 
 static const struct of_device_id vtss_dying_gasp_match[] = {
-	{ .compatible = "mscc,serval2-dying-gasp" },
+	{ .compatible = "mscc,dying-gasp" },
 	{},
 };
 MODULE_DEVICE_TABLE(of, vtss_dying_gasp_match);
