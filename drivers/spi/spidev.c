e14e0a53027589c00b89d44c4c83e4c70cd219e6
Steen Hegelund <steen.hegelund@microchip.com>
2020-03-30 10:21:57 +0200

    APPL-2990: mips: Serval2: add synce spi devices

--- drivers/spi/spidev.c
+++ drivers/spi/spidev.c
@@ -677,6 +677,7 @@ static const struct of_device_id spidev_dt_ids[] = {
 	{ .compatible = "mchp,synce_dpll" },
 	{ .compatible = "mchp,cpld" },
 	{ .compatible = "mchp,cpld_fifo" },
+	{ .compatible = "mchp,t1e1j1" },
 	{},
 };
 MODULE_DEVICE_TABLE(of, spidev_dt_ids);
